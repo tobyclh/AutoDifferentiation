362728d8ff2080a07583cd3d0c92d4be0c91f76a
Toby the potato <tobyclh@gmail.com>
2019-05-30 19:10:04 +0900

    add some stuff

--- hw-1/q1.py
+++ hw-1/q1.py
@@ -7,37 +7,40 @@ from mpl_toolkits.mplot3d import Axes3D
 
 parser = ArgumentParser()
 parser.add_argument('--theta', type=float, default=0, help='angle for the flow in degree')
-parser.add_argument('--time', type=int, default=1, help='how many time steps')
+parser.add_argument('--time', type=int, default=0, help='compute until t=#, 0 means until it converges')
 parser.add_argument('--delta_t', type=float, default=0.01, help='how many time steps')
 parser.add_argument('--pe', type=float, default=1, help='peclet number')
 parser.add_argument('--display_all', action='store_true', help='visualize all the images or just the last one')
 opt = parser.parse_args()
 
 # setup pre-defined properties
-_range = 10 # the field size
-_delta = 0.5 # = delta x = delta y
-field_size = int(_range / _delta)
 U_0 = 1
+delta_d = 10/20 #displacement delta = delta_x = delta_y
 x = np.linspace(0, 10, 20)
 y = np.linspace(0, 10, 20)
 X, Y = np.meshgrid(x, y)
+field_size = y.shape[0]
 
 # Compute the flow direction in the 2 axis 
 u = U_0 * np.cos(opt.theta*np.pi/180) #x
 v = U_0 * np.sin(opt.theta*np.pi/180) #y
 
-D = _delta/opt.pe
+D = delta_d/opt.pe
 
 #initialization
 grid = np.zeros([field_size, field_size])
 grid[9:11, 9:11] = 1
-if opt.display_all:
+
+def draw(img):
     fig = plt.figure()
     ax = Axes3D(fig)
     ax.set_zlim3d(0,1)
-    surf = ax.plot_surface(X, Y, grid, cmap=cm.coolwarm,
+    surf = ax.plot_surface(X, Y, img, cmap=cm.coolwarm,
                         linewidth=0, antialiased=False, vmax=1, vmin=0)
     plt.show()
+
+if opt.display_all:
+    draw(img)
 
 def compute_T(T, Tijm1, Tijp1, Tim1j, Tip1j):
     """compute the value of T^{n+1}_{i, j}
@@ -53,12 +56,12 @@ def compute_T(T, Tijm1, Tijp1, Tim1j, Tip1j):
         float -- T^{n+1}_{i, j}
     """
 
-    RHS = D*(Tijm1 + Tijp1 + Tim1j + Tip1j - 4 * T)/(_delta**2)
-    LHS = u*(Tip1j - Tim1j)/2/_delta + v*(Tijp1 - Tijm1)/2/_delta
+    RHS = D*(Tijm1 + Tijp1 + Tim1j + Tip1j - 4 * T)/(delta_d**2)
+    LHS = u*(Tip1j - Tim1j)/2/delta_d + v*(Tijp1 - Tijm1)/2/delta_d
     return (RHS-LHS)*opt.delta_t + T
 
 def update():
-    """update the grid inplace
+    """compute the grid for 1 time step
     
     Returns:
         field_size x field_size array -- updated output
@@ -66,20 +69,37 @@ def update():
     _grid = grid.copy()
     for i in range(field_size):
         for j in range(field_size):
-            im1 = _grid[j, i-1] if i-1 >= 0 else 0
-            ip1 = _grid[j, i+1] if i+1 < field_size else 0
-            jm1 = _grid[j-1, i] if j-1 >= 0 else 0
-            jp1 = _grid[j+1, i] if j+1 < field_size else 0
-            grid[j, i] = compute_T(_grid[j, i], jm1, jp1, im1, ip1)
-    return grid
+            im1 = grid[j, i-1] if i-1 >= 0 else 0
+            ip1 = grid[j, i+1] if i+1 < field_size else 0
+            jm1 = grid[j-1, i] if j-1 >= 0 else 0
+            jp1 = grid[j+1, i] if j+1 < field_size else 0
+            _grid[j, i] = compute_T(grid[j, i], jm1, jp1, im1, ip1)
+    return _grid
 
-n_steps = int(opt.time/opt.delta_t)
-for t in range(n_steps):
+i = 0
+if opt.time != 0:
+    #compute a fixed number of steps
+    n_steps = int(opt.time/opt.delta_t)
+    def criteria(_i, img):
+        return i>=n_steps
+else:
+    #compute until it converges
+    past_error = 1e10
+    def criteria(_i, img):
+        error = np.abs(img-grid).mean()/np.abs(grid).mean()
+        improved_rate = abs((error - past_error)/past_error)
+        past_error = error
+        return improved_rate < 1e-4
+done = False
+while not done:
     img = update()
-    if opt.display_all or t == n_steps-1:
-        fig = plt.figure()
-        ax = Axes3D(fig)
-        ax.set_zlim3d(0,1)
-        surf = ax.plot_surface(X, Y, img, cmap=cm.coolwarm,
-                        linewidth=0, antialiased=False)
-        plt.show()
+    if opt.display_all:
+        draw(img)
+    i += 1
+    result = criteria(i, img)
+    result
+    print(i, np.abs(img-grid).mean()/np.abs(grid).mean())
+    grid = img
+
+
+draw(grid)
